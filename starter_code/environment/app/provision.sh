9f14675b164c76e67de08f683a36c12b8cb536d0
nathanforester <nfeugene86@gmail.com>
2020-07-01 08:47:45 +0100

    first commit

---
+++ starter_code/environment/app/provision.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+nginx start
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+sudo npm install pm2 -g
+
+
+echo 'export DB_HOST=mongodb://192.168.10.150:27017/posts' >> ~/.bashrc
+
